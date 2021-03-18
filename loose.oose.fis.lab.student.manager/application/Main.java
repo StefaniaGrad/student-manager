c0bb2f94539a4e820c0dff98b6d5e72faedded0d
Monica Dumescu <dumescumonica@gmail.com>
2021-03-18 12:57:07 +0200

    SM-2 Added the Students Table and some dummy student list

--- loose.oose.fis.lab.student.manager/application/Main.java
+++ loose.oose.fis.lab.student.manager/application/Main.java
@@ -8,17 +8,13 @@ import javafx.scene.layout.BorderPane;
 
 public class Main extends Application {
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
+	public void start(Stage primaryStage) throws Exception {
+		   Parent root = FXMLLoader.load(getClass().getResource("fxml/view-students.xml"));
+		   primaryStage.setTitle("Student Manager");
+		   primaryStage.setScene(new Scene(root, 600, 400));
+		   primaryStage.show();
 		}
-	}
+
 	
 	public static void main(String[] args) {
 		System.out.println("hello");
